08fd1464368af7ea697ed3b67d5efc2c8b9b99de
tienchinh21 <tienchinh21.it@gmail.com>
2022-11-01 09:51:38 +0700

    fix ex2

--- baitap1/index.js
+++ baitap1/index.js
@@ -1,28 +1,109 @@
 
-let button = document.getElementById('timnghiem');
-button.addEventListener('click',function(){
-    let a = document.getElementById('a').value
-    let b = document.getElementById('b').value
+let buttonEl = document.getElementById('timnghiem');
+let numberAEl = document.getElementById('a');
+let numberBEl = document.getElementById('b');
 
-    if(a== "" || b =="") {
-        alert("Vui lòng nhập dữ liệu");
-    } else if (a != Number(a) || b != Number(b)) {
-        alert("Vui lòng nhập lại giá trị")
+/**
+ * bool
+ * true:
+ * false: false, null, undefined, "", 0
+ */
+
+buttonEl.addEventListener('click', function() {
+    let numberAValue = numberAEl.value;
+    let numberBValue = numberBEl.value;
+
+    console.log({
+        numberAValue,
+        numberBValue,
+    })
+
+    /**
+     * numberAValue = 'sdsd';
+     * numberBValue = 'sdsdsd';
+     *
+     *
+     * if (true || true) {
+     * }
+     *
+     *
+     * if (!numberAValue || !numberBValue) {
+     *   // chay
+     * }
+     */
+
+    // !false        || !false
+    if (!numberAValue || !numberBValue) {
+        // alert("Error: Vui lòng nhập dữ liệu");
+        showAlert('error', 'Vui lòng nhập dữ liệu');
     } else {
-        a = Number(a);
-        b = Number(b);
-        giaiptbacnhat(a, b);
+        numberAValue = +(numberAValue);
+        numberBValue = +(numberBValue);
+        const result = giaiptbacnhat(numberAValue, numberBValue);
+        console.log(result);
+        // alert("Success: Tính toán thành công");
+        showAlert('success', 'Tính toán thành công');
     }
-    function giaiptbacnhat(a,b) {
-        if (a == 0) return {
-            title:'phương trình vô nghiệm',
-        }; else {
-            const x = -b/a;
-            return {
-                title:'Phương trình có 1 nghiệm',
-                x:x,
-            }
-        }       
-    }
-    console.log(giaiptbacnhat(a,b));
 });
+
+/**
+ * toUpperCase: chuyển chữ viết thường thành chữ viết hoa -> String
+ * toLowerCase: chuyển chữ cái viết hoa thành viết thường. -> String
+ * toCapitalize
+ *
+ * const number = {};
+ * number.methodName = function() {
+ * }
+ *
+ * Đối với Đối tượng mặc định của javascript thì mình cần làm như sau
+ * Number, Object, String, Array, Function
+ * .prototype.methodName = function() {
+ * }
+ */
+
+/**
+ *         0 1 2 3
+ * string: t y p e
+ * string[0] = t
+ * t.toUpperCase() => T
+ *
+ *         0 1 2 3 4 5 6
+ * string: t y p e         => endIndex = 3, length = 4 - 1
+ * string: s u c c e s s   => endIndex = 6, length = 7 - 1
+ * - Trích xuất một phần của chuỗi
+ * - Chỉ mục bắt đầu: vị trí index mà muốn bắt đầu lấy
+ * - Chỉ mục kết thúc: muốn lấy tới vị trí index nào
+ *
+ * - Muốn lấy: ype, startIndex = 1, endIndex = 3
+ * string.slice(1, string.length - 1) (1, 6)
+ * string.slice(firstIndex, length) [)
+ *
+ *         0 1 2 3
+ * string: t y p e
+ * lấy: ype (1, 4) -> 1-2
+ */
+String.prototype.toCapitalize = function(string) {
+    let firstChar = string[0].toUpperCase();
+    let newString = firstChar + string.slice(1, string.length);
+    return newString;
+}
+
+function showAlert(type, message) {
+    const newType = String.prototype.toCapitalize(type);
+    const newAlert = newType + ": "+ message;
+    alert(newAlert);
+}
+
+function giaiptbacnhat(a, b) {
+    if (a == 0) {
+        return {
+            title:'phương trình vô nghiệm',
+        }
+    } else {
+        const x = -b/a;
+        return {
+            title:'Phương trình có 1 nghiệm',
+            x:x,
+        }
+    }
+}
--- baitap2/main.js
+++ baitap2/main.js
@@ -1,50 +1,68 @@
-let buttonEl =document.getElementById('btn')
-btn.addEventListener('click',function(){
-    let a =document.getElementById('a').value
-    let b =document.getElementById('b').value
-    let c =document.getElementById('c').value
+let buttonEl  =document.getElementById('btn');
+let numberAEl =document.getElementById('a');
+let numberBEl =document.getElementById('b');
+let numberCEl =document.getElementById('c');
 
-    if (a == "" || b == "") {
-        alert("Vui lòng nhập dữ liệu");
-    } else if (a != Number(a) || b != Number(b) || c != Number(c)) {
-        alert("Vui lòng nhập lại giá trị");
-    }else {
-        giaiptbac2(a,b,c)
+
+btn.addEventListener('click', function() {
+    let numberAValue = numberAEl.value;
+    let numberBValue = numberBEl.value;
+    let numberCValue = numberCEl.value;
+
+
+    if (!numberAValue || !numberBValue) {
+        showAlert('error', 'Vui lòng nhập dữ liệu');
+    } else {
+        numberAValue = +(numberAValue);
+        numberBValue = +(numberBValue);
+        numberCValue = +(numberCValue);
+        const result = giaiptbac2(numberAValue, numberBValue, numberCValue);
+        console.log(result);  
+        showAlert('success','Tính toán kết quả thành công');     
     }
-    function giaiptbac2(a,b,c) {   
-        function giaiptbacnhat(b, c){
-            const x = -b/c;
-            return {
-                // title:'Phương trình có 1 nghiệm',
-                x:x,
-            }
-        } 
-        if (a == 0) return {
+});
+String.prototype.toCapitalize = function(string) {
+    let firstChar = string[0].toUpperCase();
+    let newString = firstChar + string.slice(1, string.length);
+    return newString;
+}
+function showAlert(type, message) {
+    const newType = String.prototype.toCapitalize(type);
+    const newAlert = newType + ": "+ message;
+    alert(newAlert);
+}
+function giaiptbacnhat(b, c) {
+    const x = -b/c;
+    return {
+    x:x,
+    }
+} 
+function giaiptbac2(a, b, c) {   
+    if (a == 0) {
+        return {
             title:'Phương trình bậc 1',
             x: giaiptbacnhat(b, c),
-        };
-
-        const delta = b*b-4*a*c;
-
-        if(delta > 0) {
-            const x1 = -b + Math.sqrt(delta)/2*a;
-            const x2 = -b - Math.sqrt(delta)/2*a;
-            return {
-                title: 'Phương trình có 2 nghiệm',
-                x1: x1,
-                x2: x2, 
-            };
-        } else if (delta === 0) {
-                const x =-b/2*a;
-                return {
-                title :'Phương trình có nghiệm kép',
-                x: x,
-                }
-        } else {
-            return {
-                title: 'Phương trình vô nghiệm' 
-            }
         }
     }
-    console.log(giaiptbac2(a,b,c));
-});
+    const delta = b*b-4*a*c;
+    
+    if (delta > 0) {
+        const x1 = -b + Math.sqrt(delta)/2*a;
+        const x2 = -b - Math.sqrt(delta)/2*a;
+        return {
+            title: 'Phương trình có 2 nghiệm',
+            x1: x1,
+            x2: x2, 
+        }
+    } else if (delta === 0) {
+        const x =-b/2*a;
+        return {
+            title :'Phương trình có nghiệm kép',
+            x: x,
+        }
+    } else {
+        return {
+            title: 'Phương trình vô nghiệm', 
+        }
+    }
+}
